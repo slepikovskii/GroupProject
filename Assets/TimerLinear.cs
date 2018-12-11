0e2cca0eec572fe2ee64355b949c5ce8d5e9be50
Elena Gorshkova <44033158+ilixilia@users.noreply.github.com>
2018-12-12 01:10:49 +0300

    comments

--- Assets/TimerLinear.cs
+++ Assets/TimerLinear.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+//Betül 
 public class TimerLinear : MonoBehaviour {
 
     Image FillImg;
