e42c50325a5920947ba587fe351fb39fcef1b7d5
betts <betulsunter@gmail.com>
2018-12-12 00:38:38 +0200

    Update Sound.cs

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//Svyatoslav
+//Artem
 public class Sound : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
