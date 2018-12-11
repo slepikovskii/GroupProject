2b72b4f823a83f532fa6c89857a1e1b40dba41a2
Elena Gorshkova <44033158+ilixilia@users.noreply.github.com>
2018-12-12 00:51:57 +0300

    comments

--- Assets/Scripts/MusicScript.cs
+++ Assets/Scripts/MusicScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+//Svyatoslav
 public class Musicscript : MonoBehaviour
 {
     public AudioClip MusicClip;
